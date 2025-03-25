7ffaf96cc34ac8e00368ebd12ac557f39794ede0
Nikhilbhor96 <nikhilbhor96@gmail.com>
2025-03-25 17:09:48 +0530

    Update Q14.php

--- PHP/Q14.php
+++ PHP/Q14.php
@@ -14,19 +14,23 @@ Ans::
 <?php
 $xml = new SimpleXMLElement('<movies/>');
 
-$classical = $xml->addChild('Category');
-$classical->addChild('Movie Name', 'The Godfather');
-$classical->addChild('Release Year', '1972');
+$classical = $xml->addChild('Classical');
+$movie1 = $classical->addChild('Movie');
+$movie1->addChild('MovieName', 'The Godfather');
+$movie1->addChild('ReleaseYear', '1972');
 
-$action = $xml->addChild('Category');
-$action->addChild('Movie Name', 'Die Hard');
-$action->addChild('Release Year', '1988');
+$action = $xml->addChild('Action');
+$movie2 = $action->addChild('Movie');
+$movie2->addChild('MovieName', 'Die Hard');
+$movie2->addChild('ReleaseYear', '1988');
 
-$horror = $xml->addChild('Category');
-$horror->addChild('Movie Name', 'The Exorcist');
-$horror->addChild('Release Year', '1973');
+$horror = $xml->addChild('Horror');
+$movie3 = $horror->addChild('Movie');
+$movie3->addChild('MovieName', 'The Exorcist');
+$movie3->addChild('ReleaseYear', '1973');
 
 header('Content-type: text/xml');
 echo $xml->asXML();
+
 file_put_contents('movies.xml', $xml->asXML());
 ?>
